7f51c8fa70588e12b827fb3e78eb345815ace948
prachi-porwal24 <bittuporwal24@gmail.com>
2019-06-16 10:20:07 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,12 @@
+from googlesearch import search 
+import pyqrcode
+from pyqrcode import QRCode
+urlinput=input("enter text to search")
+urllist=[]
+for i in search(urlinput, tld='com', lang='en', num=10, start=0, stop=5, pause=2) :
+	urllist.append(i)
+	print(i)
+	url=pyqrcode.create(i)
+	for j in range(5) :
+		url.svg(str(j)+".svg",scale=8)
+print(url.terminal())
